33027ae0e1ae3ffef631340471c434b3b4121042
Emily McLean <me@emilym.cl>
2024-04-10 19:35:38 +1000

    Setup project

---
+++ src/main/kotlin/mc/emilym/bytes/HumanReadableBytes.java
@@ -0,0 +1,22 @@
+package mc.emilym.bytes;
+
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.Locale;
+
+public class HumanReadableBytes {
+
+    // From https://stackoverflow.com/questions/3758606/how-can-i-convert-byte-size-into-a-human-readable-format-in-java
+    public static String si(long bytes) {
+        if (-1000 < bytes && bytes < 1000) {
+            return bytes + " B";
+        }
+        CharacterIterator ci = new StringCharacterIterator("kMGTPE");
+        while (bytes <= -999_950 || bytes >= 999_950) {
+            bytes /= 1000;
+            ci.next();
+        }
+        return String.format("%.1f %cB", bytes / 1000.0, ci.current());
+    }
+
+}
